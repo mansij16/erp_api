8e026061468030aa4070b506045cd3c2aa039f2a
mansij16 <mansi@beyond-blueprints.com>
2026-01-19 11:22:34 +0530

    fixed the port

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ mongoose
   })
   .catch((err) => console.error("MongoDB connection error:", err));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
