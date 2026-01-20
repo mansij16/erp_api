dfc6ee4598f99af084839e0cfbbc6c7bf7348e92
mansij16 <mansi@beyond-blueprints.com>
2026-01-20 13:03:26 +0530

    added allowedHeaders for CORS error

--- server.js
+++ server.js
@@ -59,7 +59,13 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "Accept",
+    "Origin",
+  ],
 };
 
 
